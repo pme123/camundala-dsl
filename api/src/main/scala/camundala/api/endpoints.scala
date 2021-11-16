2d762bcd98dd7c006d6fa27b7171ad3f5827bdf2
Pascal Mengelt <pascal.mengelt@gmail.com>
2021-11-16 18:09:21 +0100

    removed api_docs

--- api/src/main/scala/camundala/api/endpoints.scala
+++ api/src/main/scala/camundala/api/endpoints.scala
@@ -13,23 +13,23 @@ import java.util.Base64
 type ExampleName = String
 
 case class RequestErrorOutput(
-                               statusCode: StatusCode,
-                               examples: Map[ExampleName, CamundaError] = Map.empty
-                             )
+    statusCode: StatusCode,
+    examples: Map[ExampleName, CamundaError] = Map.empty
+)
 
 case class CamundaError(
-                         `type`: String = "SomeExceptionClass",
-                         message: String = "a detailed message"
-                       )
+    `type`: String = "SomeExceptionClass",
+    message: String = "a detailed message"
+)
 
 case class CamundaAuthError(
-                             private val `type`: String = "AuthorizationException",
-                             message: String = "a detailed message",
-                             userId: String = "jonny",
-                             permissionName: String = "DELETE",
-                             resourceName: String = "User",
-                             resourceId: String = "Mary"
-                           )
+    private val `type`: String = "AuthorizationException",
+    message: String = "a detailed message",
+    userId: String = "jonny",
+    permissionName: String = "DELETE",
+    resourceName: String = "User",
+    resourceId: String = "Mary"
+)
 
 sealed trait CamundaVariable
 
@@ -50,8 +50,8 @@ object CamundaVariable:
   import reflect.Selectable.reflectiveSelectable
 
   def toCamunda[T <: Product: Encoder: Decoder: Schema](
-                                                         product: T
-                                                       ): Map[ExampleName, CamundaVariable] =
+      product: T
+  ): Map[ExampleName, CamundaVariable] =
     product.productElementNames
       .zip(product.productIterator)
       .flatMap {
@@ -95,57 +95,56 @@ object CamundaVariable:
       .toMap
 
   case class CString(value: String, private val `type`: String = "String")
-    extends CamundaVariable
+      extends CamundaVariable
   case class CInteger(value: Int, private val `type`: String = "Integer")
-    extends CamundaVariable
+      extends CamundaVariable
   case class CLong(value: Long, private val `type`: String = "Long")
-    extends CamundaVariable
+      extends CamundaVariable
   case class CBoolean(value: Boolean, private val `type`: String = "Boolean")
-    extends CamundaVariable
+      extends CamundaVariable
 
   case class CDouble(value: Double, private val `type`: String = "Double")
-    extends CamundaVariable
+      extends CamundaVariable
 
   case class CFile(
-                    @description("The File's content as Base64 encoded String.")
-                    value: String,
-                    valueInfo: CFileValueInfo,
-                    private val `type`: String = "File"
-                  ) extends CamundaVariable
+      @description("The File's content as Base64 encoded String.")
+      value: String,
+      valueInfo: CFileValueInfo,
+      private val `type`: String = "File"
+  ) extends CamundaVariable
 
   case class CFileValueInfo(
-                             filename: String,
-                             mimetype: Option[String]
-                           )
+      filename: String,
+      mimetype: Option[String]
+  )
 
   case class CEnum(value: String, private val `type`: String = "String")
-    extends CamundaVariable
+      extends CamundaVariable
 
   case class CJson(value: String, private val `type`: String = "Json")
-    extends CamundaVariable
+      extends CamundaVariable
 
 case class NoInput()
 
 case class FileInOut(
-                      fileName: String,
-                      @description("The content of the File as a Byte Array.")
-                      content: Array[Byte],
-                      mimeType: Option[String]
-                    ):
+    fileName: String,
+    @description("The content of the File as a Byte Array.")
+    content: Array[Byte],
+    mimeType: Option[String]
+):
   lazy val contentAsBase64 = Base64.getEncoder.encodeToString(content)
 
 @description(
   "A JSON object with the following properties: (at least an empty JSON object {} or an empty request body)"
 )
-case class StartProcessIn[T <: Product](
-                                         _api_doc: Option[T],
-                                         // use the description of the object
-                                         variables: Map[ExampleName, CamundaVariable],
-                                         @description("The business key of the process instance.")
-                                         businessKey: Option[String] = Some("example-businesskey"),
-                                         @description("Set to false will not return the Process Variables.")
-                                         withVariablesInReturn: Boolean = true
-                                       )
+case class StartProcessIn(
+    // use the description of the object
+    variables: Map[ExampleName, CamundaVariable],
+    @description("The business key of the process instance.")
+    businessKey: Option[String] = Some("example-businesskey"),
+    @description("Set to false will not return the Process Variables.")
+    withVariablesInReturn: Boolean = true
+)
 
 /*
 @endpointInput("task/{taskId}/form-variables")
@@ -161,37 +160,35 @@ case class GetTaskFormVariablesPath(
 @description(
   "A JSON object with the following properties: (at least an empty JSON object {} or an empty request body)"
 )
-case class CompleteTaskIn[T <: Product](
-                                         _api_doc: Option[T],
-                                         // use the description of the object
-                                         variables: Map[ExampleName, CamundaVariable],
-                                         @description(
-                                           "Set to false will not return the Process Variables and the Result Status is 204."
-                                         )
-                                         withVariablesInReturn: Boolean = true
-                                       )
+case class CompleteTaskIn(
+    // use the description of the object
+    variables: Map[ExampleName, CamundaVariable],
+    @description(
+      "Set to false will not return the Process Variables and the Result Status is 204."
+    )
+    withVariablesInReturn: Boolean = true
+)
 
 @description(
   "A JSON object with the following properties"
 )
 case class GetActiveTaskIn(
-                            @description(
-                              """The id of the process - you want to get the active tasks.
-                                |> This is the result id of the `StartProcessOut`""".stripMargin
-                            )
-                            processInstanceId: String = "{{processInstanceId}}",
-                            @description("We are only interested in the active Task(s)")
-                            active: Boolean = true
-                          )
+    @description(
+      """The id of the process - you want to get the active tasks.
+        |> This is the result id of the `StartProcessOut`""".stripMargin
+    )
+    processInstanceId: String = "{{processInstanceId}}",
+    @description("We are only interested in the active Task(s)")
+    active: Boolean = true
+)
 
 @description(
   "A JSON object with the following properties:"
 )
-case class EvaluateDecisionIn[T <: Product](
-                                             _api_doc: Option[T],
-                                             // use the description of the object
-                                             variables: Map[ExampleName, CamundaVariable]
-                                           )
+case class EvaluateDecisionIn(
+    // use the description of the object
+    variables: Map[ExampleName, CamundaVariable]
+)
 
 case class RequestInput[T](examples: Map[ExampleName, T]):
   def :+(label: String, example: T) =
@@ -207,10 +204,10 @@ object RequestInput:
     new RequestInput[T](Map.empty)
 
 case class RequestOutput[T](
-                             statusCode: StatusCode,
-                             hasManyResults: Boolean,
-                             examples: Map[ExampleName, T]
-                           ):
+    statusCode: StatusCode,
+    hasManyResults: Boolean,
+    examples: Map[ExampleName, T]
+):
   lazy val noOutdput =
     examples.isEmpty
   def :+(label: String, example: T) =
@@ -222,34 +219,34 @@ object RequestOutput {
     RequestOutput(StatusCode.Ok, false, Map.empty)
 
   def apply[Out <: Product](
-                             statusCode: StatusCode,
-                             hasManyResults: Boolean,
-                             example: Out
-                           ): RequestOutput[Out] =
+      statusCode: StatusCode,
+      hasManyResults: Boolean,
+      example: Out
+  ): RequestOutput[Out] =
     RequestOutput(statusCode, hasManyResults, Map("standard" -> example))
 
   def ok[Out <: Product](
-                          example: Out,
-                          hasManyResults: Boolean
-                        ): RequestOutput[Out] =
+      example: Out,
+      hasManyResults: Boolean
+  ): RequestOutput[Out] =
     apply(StatusCode.Ok, hasManyResults, example)
 
   def created[Out <: Product](
-                               example: Out,
-                               hasManyResults: Boolean
-                             ): RequestOutput[Out] =
+      example: Out,
+      hasManyResults: Boolean
+  ): RequestOutput[Out] =
     apply(StatusCode.Created, hasManyResults, example)
 
   def ok[Out <: Product](
-                          examples: Map[ExampleName, Out],
-                          hasManyResults: Boolean
-                        ): RequestOutput[Out] =
+      examples: Map[ExampleName, Out],
+      hasManyResults: Boolean
+  ): RequestOutput[Out] =
     RequestOutput(StatusCode.Ok, hasManyResults, examples)
 
   def created[Out <: Product](
-                               examples: Map[ExampleName, Out],
-                               hasManyResults: Boolean
-                             ): RequestOutput[Out] =
+      examples: Map[ExampleName, Out],
+      hasManyResults: Boolean
+  ): RequestOutput[Out] =
     RequestOutput(StatusCode.Created, hasManyResults, examples)
 
 }
@@ -260,62 +257,61 @@ case class NoOutput()
   """A JSON object representing the newly created process instance.
     |""".stripMargin
 )
-case class StartProcessOut[T <: Product](
-                                          _api_doc: Option[T],
-                                          @description(
-                                            "The Process Variables - Be aware that returns everything stored in the Process."
-                                          )
-                                          variables: Map[ExampleName, CamundaVariable],
-                                          @description(
-                                            """The id of the process instance.
-                                              |
-                                              |> **Postman**:
-                                              |>
-                                              |> Add the following to the tests to set the `processInstanceId`:
-                                              |>
-                                              |>```
-                                              |let processInstanceId = pm.response.json().id
-                                              |console.log("processInstanceId: " + processInstanceId)
-                                              |pm.collectionVariables.set("processInstanceId", processInstanceId)
-                                              |>```
-                                              |""".stripMargin
-                                          )
-                                          id: String = "f150c3f1-13f5-11ec-936e-0242ac1d0007",
-                                          @description("The id of the process definition.")
-                                          definitionId: String =
-                                          "processDefinitionKey:1:6fe66514-12ea-11ec-936e-0242ac1d0007",
-                                          @description("The business key of the process instance.")
-                                          businessKey: Option[String] = Some("example-businesskey")
-                                        )
+case class StartProcessOut(
+    @description(
+      "The Process Variables - Be aware that returns everything stored in the Process."
+    )
+    variables: Map[ExampleName, CamundaVariable],
+    @description(
+      """The id of the process instance.
+        |
+        |> **Postman**:
+        |>
+        |> Add the following to the tests to set the `processInstanceId`:
+        |>
+        |>```
+        |let processInstanceId = pm.response.json().id
+        |console.log("processInstanceId: " + processInstanceId)
+        |pm.collectionVariables.set("processInstanceId", processInstanceId)
+        |>```
+        |""".stripMargin
+    )
+    id: String = "f150c3f1-13f5-11ec-936e-0242ac1d0007",
+    @description("The id of the process definition.")
+    definitionId: String =
+      "processDefinitionKey:1:6fe66514-12ea-11ec-936e-0242ac1d0007",
+    @description("The business key of the process instance.")
+    businessKey: Option[String] = Some("example-businesskey")
+)
 
 @description("A JSON object representing the newly created process instance.")
 case class CompleteTaskOut[T <: Product](
-                                          @description(
-                                            "The Process Variables - Be aware that returns everything stored in the Process."
-                                          )
-                                          variables: T
-                                        )
+    @description(
+      "The Process Variables - Be aware that returns everything stored in the Process."
+    )
+    variables: T
+)
 
 @description("A JSON object representing the newly created process instance.")
 case class GetActiveTaskOut(
-                             @description(
-                               """The Task Id you need to complete Task
-                                 |
-                                 |> **Postman**:
-                                 |>
-                                 |> Add the following to the tests to set the `taskId`:
-                                 |>
-                                 |>```
-                                 |let taskId = pm.response.json()[0].id
-                                 |console.log("taskId: " + taskId)
-                                 |pm.collectionVariables.set("taskId", taskId)
-                                 |>```
-                                 |>
-                                 |> This returns an Array!
-                                 |""".stripMargin
-                             )
-                             id: String = "f150c3f1-13f5-11ec-936e-0242ac1d0007"
-                           )
+    @description(
+      """The Task Id you need to complete Task
+        |
+        |> **Postman**:
+        |>
+        |> Add the following to the tests to set the `taskId`:
+        |>
+        |>```
+        |let taskId = pm.response.json()[0].id
+        |console.log("taskId: " + taskId)
+        |pm.collectionVariables.set("taskId", taskId)
+        |>```
+        |>
+        |> This returns an Array!
+        |""".stripMargin
+    )
+    id: String = "f150c3f1-13f5-11ec-936e-0242ac1d0007"
+)
 
 object endpoints:
 
